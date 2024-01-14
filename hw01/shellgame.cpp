a79fd3c504e2c71f89ec259c9d6e6f3c6f41caaa
tiffanygan <tiffanygan2015@gmail.com>
2024-01-13 22:55:50 -0800

    initial commit from Tiffany

---
+++ hw01/shellgame.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+//    freopen(".in", "r", stdin);
+//    freopen(".out", "w", stdout);
+
+    cout << "hello world";
+
+    return 0;
+}
